2a626bd93dc60e8fd6d01f48840d11ac0499b417
Sh.Miraki <Sh.Miraki@MIRAKI-SH>
2020-09-13 09:12:30 +0430

    init project

---
+++ src/PublicApi/CatalogItemEndpoints/Create.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Threading;
+using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Threading.Tasks;
+using Microsoft.eShopWeb.ApplicationCore.Constants;
+
+namespace Microsoft.eShopWeb.PublicApi.CatalogItemEndpoints
+{
+
+    [Authorize(Roles = AuthorizationConstants.Roles.ADMINISTRATORS, AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class Create : BaseAsyncEndpoint<CreateCatalogItemRequest, CreateCatalogItemResponse>
+    {
+        private readonly IAsyncRepository<CatalogItem> _itemRepository;
+        private readonly IUriComposer _uriComposer;
+
+        public Create(IAsyncRepository<CatalogItem> itemRepository, IUriComposer uriComposer)
+        {
+            _itemRepository = itemRepository;
+            _uriComposer = uriComposer;
+        }
+
+        [HttpPost("api/catalog-items")]
+        [SwaggerOperation(
+            Summary = "Creates a new Catalog Item",
+            Description = "Creates a new Catalog Item",
+            OperationId = "catalog-items.create",
+            Tags = new[] { "CatalogItemEndpoints" })
+        ]
+        public override async Task<ActionResult<CreateCatalogItemResponse>> HandleAsync(CreateCatalogItemRequest request, CancellationToken cancellationToken)
+        {
+            var response = new CreateCatalogItemResponse(request.CorrelationId());
+
+            var newItem = new CatalogItem(request.CatalogTypeId, request.CatalogBrandId, request.Description, request.Name, request.Price, request.PictureUri);
+
+            newItem = await _itemRepository.AddAsync(newItem);
+
+            var dto = new CatalogItemDto
+            {
+                Id = newItem.Id,
+                CatalogBrandId = newItem.CatalogBrandId,
+                CatalogTypeId = newItem.CatalogTypeId,
+                Description = newItem.Description,
+                Name = newItem.Name,
+                PictureUri = _uriComposer.ComposePicUri(newItem.PictureUri),
+                Price = newItem.Price
+            };
+            response.CatalogItem = dto;
+            return response;
+        }
+
+        
+    }
+}
